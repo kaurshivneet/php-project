20117c90917ded1480e91f852a7770a2f206ce8b
kaurshivneet <85763236+kaurshivneet@users.noreply.github.com>
2022-03-06 19:51:59 -0500

    Shivneet kaur(2110438)   2022-02-26       completed homeapge

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <!DOCTYPE html>
 <?php
+#Revision history
+#Shivneet kaur(2110438)   2022-02-20       Created NetBeans project and empty folders
+#Shivneet kaur(2110438)   2022-02-21       Created HomePage and set all the images and create php functions 
+#Shivneet kaur(2110438)   2022-02-22       written description about the website and set the logo
+#Shivneet kaur(2110438)   2022-02-23       Created Buying page with errors
+#Shivneet kaur(2110438)   2022-02-24       Removed errors of buying page
+#Shivneet kaur(2110438)   2022-02-26       completed homeapge and buying page
+#Shivneet kaur(2110438)   2022-02-27       Created orders page 
+#Shivneet kaur(2110438)   2022-03-01       Fixed the error of orders page like json decode
+#Shivneet kaur(2110438)   2022-03-03       Reviewed the all project
+#Shivneet kaur(2110438)   2022-03-05       Completed the cheatsheet of project
+#Shivneet kaur(2110438)   2022-03-06       Completed  the project 
+
+
+
+
+
+
+
+
 include_once 'PHPfunctions.php';
 define("PICTURE_1", FOLDER_PICTURES . "book0.jpg");
 define("PICTURE_2", FOLDER_PICTURES . "book10.jpg");
